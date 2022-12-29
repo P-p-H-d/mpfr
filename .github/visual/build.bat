9b3442b10481d91b01ab640be2a233093d0b2a42
Patrick Pelissier <patrick.pelissier@gmail.com>
2022-12-29 18:34:37 +0100

    Adding Continuous Integration script. (Avoid conversion to DOS text format on Windows on checkout)

---
+++ .github/visual/build.bat
@@ -0,0 +1,15 @@
+REM It shall be run from ROOTSRC
+REM Overwrite ports of VCPKG ro reuse its build infrastructure
+REM with latest version of MPFR
+xcopy /y .github\visual\tst.patch C:\vcpkg\ports\mpfr
+xcopy /y .github\visual\portfile.cmake C:\vcpkg\ports\mpfr
+REM Run install of mpfr by VCPKG
+cd C:\vcpkg
+vcpkg install mpfr:x64-windows --head
+set "MyReturnCode=%ERRORLEVEL%"
+REM Display outputs of build
+type C:\vcpkg\buildtrees\mpfr\config-x64-windows-dbg-out.log
+type C:\vcpkg\buildtrees\mpfr\config-x64-windows-dbg-err.log
+type C:\vcpkg\buildtrees\mpfr\build-x64-windows-dbg-out.log
+type C:\vcpkg\buildtrees\mpfr\build-x64-windows-dbg-err.log
+exit /B %MyReturnCode%
