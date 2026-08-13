P-p-H-d/mpfr

commit 8595dfb53e68593db9a7358c4dcf0ab27277866f

[acinclude.m4] Fixed the va_copy test (for developers).
Avoid a compilation failure with GCC and
  CFLAGS="-O2 -Werror -Wno-error=analyzer-div-by-zero -fanalyzer"

Due to the fact that ap2 was uninitialized, the test failed, so that
MPFR incorrectly detected that va_copy was not supported, yielding a
redefinition of va_copy in "vasprintf.c".

acinclude.m4
@@ -137,19 +137,27 @@ AC_TYPE_UINTPTR_T
 
 dnl va_copy macro
 AC_MSG_CHECKING([how to copy va_list])
-AC_LINK_IFELSE([AC_LANG_PROGRAM([[
+AC_LINK_IFELSE([AC_LANG_SOURCE([[
 #include <stdarg.h>
-]], [[
-   va_list ap1, ap2;
-   va_copy(ap1, ap2);
+void f (va_list ap)
+{
+  va_list ap2;
+  va_copy (ap2, ap);
+  va_end (ap2);
+}
+int main (void) { return 0; }
 ]])], [
    AC_MSG_RESULT([va_copy])
    AC_DEFINE(HAVE_VA_COPY)
-], [AC_LINK_IFELSE([AC_LANG_PROGRAM([[
+], [AC_LINK_IFELSE([AC_LANG_SOURCE([[
 #include <stdarg.h>
-]], [[
-   va_list ap1, ap2;
-   __va_copy(ap1, ap2);
+void f (va_list ap)
+{
+  va_list ap2;
+  __va_copy (ap2, ap);
+  va_end (ap2);
+}
+int main (void) { return 0; }
 ]])], [AC_DEFINE([HAVE___VA_COPY]) AC_MSG_RESULT([__va_copy])],
    [AC_MSG_RESULT([memcpy])])])
 

commit ec384ebf2017525eb5c40cc1d807251f7adbe2fa

[acinclude.m4] Fixed the va_copy test (2).
Fix "make check" failures with CFLAGS="-Wmissing-prototypes", as
it makes the va_copy test fail.

With CFLAGS="-Wmissing-prototypes -Werror", the issue was detected
at build time.

Regression from commit 8595dfb53e68593db9a7358c4dcf0ab27277866f.

Note that compiler switches like -Wmissing-prototypes should now be
in MPFR_EXTRA_CFLAGS rather than CFLAGS in order to avoid configure
tests from being affected. But we should also avoid regressions
(even though this is for developers).

acinclude.m4
@@ -139,6 +139,7 @@ dnl va_copy macro
 AC_MSG_CHECKING([how to copy va_list])
 AC_LINK_IFELSE([AC_LANG_SOURCE([[
 #include <stdarg.h>
+void f (va_list);
 void f (va_list ap)
 {
   va_list ap2;
@@ -151,6 +152,7 @@ int main (void) { return 0; }
    AC_DEFINE(HAVE_VA_COPY)
 ], [AC_LINK_IFELSE([AC_LANG_SOURCE([[
 #include <stdarg.h>
+void f (va_list);
 void f (va_list ap)
 {
   va_list ap2;